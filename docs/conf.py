8e45328a572964eb53545bd2c1cf8815a38d6cdb
Cihan Uyanik <cihanuyanik34@gmail.com>
2023-12-12 16:22:12 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 html_context = {
     "display_github": True,  # Integrate GitHub
